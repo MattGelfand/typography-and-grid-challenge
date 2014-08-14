8c7c2901e5bdd47313d45558331de52c0ecc960f
Matt Gelfand <matthew.gelfand@gmail.com>
2014-08-14 17:12:00 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    baseliner = new Baseliner({'gridColor': 'grey', 'gridHeight': 16 });
+};
+
+$(document).ready(function(){
+	$('.row > div').on('click', function(){
+	$(this).find('ul').toggleClass('visible');
+	return false; 
+	});
+});
