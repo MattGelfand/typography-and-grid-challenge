ea4624cbddc2dc158780192f6992b3bf6565904c
Matt Gelfand <matthew.gelfand@gmail.com>
2014-08-19 17:01:02 -0400

    start design for mobile

--- js/main.js
+++ js/main.js
@@ -1,10 +1,18 @@
-window.onload = function() {
-    baseliner = new Baseliner({'gridColor': 'grey', 'gridHeight': 16 });
+/*window.onload = function() {
+    baseliner = new Baseliner({'gridColor': 'grey', 'gridHeight': 28 });
 };
-
+*/
 $(document).ready(function(){
 	$('.row > div').on('click', function(){
 	$(this).find('ul').toggleClass('visible');
 	return false; 
 	});
 });
+
+/*
+$(document).ready(function(){
+	$('a').on('click', function(){
+	return false; 
+	});
+});
+*/
